ba66c154ca4e7bf48444c827db7261bb9e1998a5
Rasmus Buurman <rasmus@rbuurman.de>
2025-06-09 01:56:16 +0200

    Formatting

--- include/util/arena.h
+++ include/util/arena.h
@@ -12,18 +12,18 @@
 typedef struct ArenaRegion ArenaRegion;
 
 struct ArenaRegion {
-	ArenaRegion *next;
-	size_t count;
-	size_t cap;
-	uintptr_t data[];
+    ArenaRegion *next;
+    size_t count;
+    size_t cap;
+    uintptr_t data[];
 };
 
 typedef struct {
-	ArenaRegion *begin, *end;
+    ArenaRegion *begin, *end;
 } Arena;
 
 #ifndef ARENA_REGION_DEFAULT_CAPACITY
-#define ARENA_REGION_DEFAULT_CAPACITY (8*1024)
+#define ARENA_REGION_DEFAULT_CAPACITY (8 * 1024)
 #endif // ARENA_REGION_DEFAULT_CAPACITY
 
 ArenaRegion *arena_region(size_t cap);
