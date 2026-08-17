4e39e58ad21ca71fcd5b8bb73d67139fe35e2114
Rasmus Buurman <rasmus@rbuurman.de>
2026-08-17 02:34:09 +0200

    Derive Clone for AST types

--- src/ast.rs
+++ src/ast.rs
@@ -1,60 +1,60 @@
 use crate::span::Span;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Program {
     pub function: FunctionDefinition,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct FunctionDefinition {
     pub name: Identifier,
     pub body: Expression,
     pub span: Span,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Identifier {
     pub identifier: String,
     pub span: Span,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Expression {
     pub kind: ExpressionKind,
     pub span: Span,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum ExpressionKind {
     Constant(IntegerLiteral),
     Call(CallKind),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct IntegerLiteral {
     pub value: i64,
     pub span: Span,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum CallKind {
     Builtin(BuiltinCall),
     Function(FunctionCall),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct BuiltinCall {
     pub kind: BuiltinCallKind,
     pub span: Span,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum BuiltinCallKind {
     BitwiseNot { arg: Box<Expression> },
     Negation { arg: Box<Expression> },
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct FunctionCall {
     pub function_name: Identifier,
     pub args: Vec<Expression>,
