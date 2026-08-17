87a8e3eb2e053bcfb40f7cf3f0a8a7b40299d5cd
Rasmus Buurman <rasmus@rbuurman.de>
2026-08-17 02:35:52 +0200

    Derive Clone for assembly AST types

--- src/codegen/asm.rs
+++ src/codegen/asm.rs
@@ -1,21 +1,21 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Program {
     pub function: FunctionDefinition,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct FunctionDefinition {
     pub name: String,
     pub instructions: Vec<Instruction>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Instruction {
     Mov { src: Operand, dst: Operand },
     Ret,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Operand {
     Immediate(i64),
     Register,
